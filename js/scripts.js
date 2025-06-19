a47046403f354a59f3f69dbd36ba3571b302eff4
Advik Chaudhary <advikchau@gmail.com>
2025-06-19 23:40:34 +0800

    refresH

--- js/scripts.js
+++ js/scripts.js
@@ -1,193 +1,46 @@
-import * as THREE from "./three.module.min.js";
-// import {OrbitControls} from "/js/OrbitControls.js";
+const clickableDivs = document.querySelectorAll('.project-button, .social-button');
 
-document.body.style.zoom = 1.0 // reset zoom
-window.addEventListener( 'resize', onWindowResize, false );
-window.addEventListener( 'refresh', onWindowResize, false );
-document.getElementById("about").onclick = function() {hideOnClick("about-div")};
-document.getElementById("projects").onclick = function() {hideOnClick("projects-div")};
+clickableDivs.forEach(div => {
+  div.style.cursor = 'pointer';
+});
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setPixelRatio(window.devicePixelRatio);  
-document.body.appendChild( renderer.domElement );
-
-const geometry = new THREE.TorusKnotGeometry( 10, 3, 100, 30 ); 
-const geometry_2 = new THREE.IcosahedronGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial( { color: 0x004400, wireframe: true } );
-const cube = new THREE.Mesh( geometry, material );
-const g2  = new THREE.Mesh( geometry_2, material )
-scene.add( cube );
-// scene.add(g2);
-
-camera.position.z = 10;
-
-
-function animate() {
-	requestAnimationFrame( animate );
-	renderer.render( scene, camera );
-  cube.rotation.x += 0.001;
-  cube.rotation.y += 0.001;
-
-  g2.rotation.x += 0.001;
-  g2.rotation.y += 0.001;
-  camera.rotation.z += 0.001;
-
-}
-animate();
-
-
-function onWindowResize(){
-
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize( window.innerWidth, window.innerHeight );
-
-}
-
-// $("#clicked_link").click( function() {
-//   $("#login").slidedown("slow");
-//   return false;
-// });
-
-function hideOnClick(someElementId) {
-  let div = document.getElementById(someElementId);
-  // if (div.style.display === "none") {
-  //   div.style.display = "block";
-  // } else {
-  //   div.style.display = "none";
-  // }
-  // return false;
-  div.classList.toggle('show');
-}
-
-
-// var CANVAS_WIDTH = window.innerWidth;
-// // var CANVAS_HEIGHT = 100
-// var canvasHeight = window.innerHeight;
-// document.documentElement.style.setProperty("--three-canvas-h", canvasHeight);
-
-// var threeCanvas = document.getElementById("mainText");
-// var renderer = new THREE.WebGLRenderer({alpha: true});
-// renderer.setSize(CANVAS_WIDTH, canvasHeight)
-// threeCanvas.appendChild(renderer.domElement);
-
-// var scene = new THREE.Scene();
-// var camera = new THREE.PerspectiveCamera( 50, CANVAS_WIDTH / canvasHeight, 0.01, 10000 );
-
-// scene.background = new THREE.Color("#e3dcc1");
-
-
-// // const controls = new OrbitControls(camera, renderer.domElement)
-
-
-// // const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-// // const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-// // const cube = new THREE.Mesh( geometry, material );
-// // scene.add( cube );
-// // camera.lookAt(cube.position);
-
-// // camera.position.z = 2;
-
-// var planeGeometry = new THREE.PlaneBufferGeometry(1000, 1000, 200, 200);
-// var planeMaterial = new THREE.MeshBasicMaterial({color: 0xab09f6, wireframe: true});
-// planeMaterial.wireframeLinewidth = 5;
-// var plane = new THREE.Mesh(planeGeometry, planeMaterial);
-// plane.material.transparent = true;
-// plane.material.opacity = 1;
-
-// plane.rotation.x = -0.5 * Math.PI;
-// plane.position.set(0, 0, 0);
-// scene.add(plane);
-
-// // camera.position.set(0, 100, 10)
-// camera.position.set(0, 100, 75)
-// camera.lookAt(scene.position);
-
-
-// // function animate() {
-// // 	requestAnimationFrame( animate );
-
-// // 	renderer.render( scene, camera );
-// // }
-// // // SINE WAVE REPEL GENERATOR EFFECT: https://codepen.io/gbnikolov/pen/QwjGPg?editors=0010
-
-// // animate();
-// var animation;
-// (function drawFrame(ts){
-//   camera.position.y = document.getElementsByClassName("mainText").scrollTop
-//     // var center = new THREE.Vector2(0,0);
-//     // animation = requestAnimationFrame(drawFrame);
-//     // var v = plane.geometry.attributes.position.array
-//     // var vLength = v.length;
+function showElement(el) {
+    document.getElementById(el + '-popup').style.display='block';
+    document.getElementById('popup-overlay').style.display='block';
     
-//     // for (var i = 0; i < vLength; i+=3) {
-//     //   var dist = new THREE.Vector2(v[i], v[i+1]).sub(center);
-//     //   var size = 15.0;
-//     //   var magnitude = 10;
-//     //   v[i+2] = Math.sin(dist.length()/-size + (ts/500)) * magnitude;
-//     // }
-//     // // plane.rotation.z +=0.01
-//     // // plane.rotation.z = document.getElementsByClassName("mainText").scrollTop
-//     // let scrollElement = document.querySelector('html');
-//     // if(scrollElement.scrollTop != 0){
-//     //   console.log(scrollElement.scrollTop)
-//     //   // plane.rotation.x = 1-(scrollElement.scrollTop-0)/(document.documentElement.scrollHeight - window.innerHeight); //normalise b/w 0 to 1 val - min / max - min
-//     //   // plane.rotation.z = (scrollElement.scrollTop-0)/(document.documentElement.scrollHeight - window.innerHeight); //normalise b/w 0 to 1 val - min / max - min
-//     //   // planeMaterial.color.r = 0.3+(scrollElement.scrollTop-0)/(document.documentElement.scrollHeight - window.innerHeight);
-//     //   // planeMaterial.color.b = 2*(scrollElement.scrollTop-0)*0.2/(document.documentElement.scrollHeight - window.innerHeight);
-//     //   // planeMaterial.color.g = 0.6-(scplane.rotation.x = 1-(scrollElement.scrollTop-0)/(document.documentElement.scrollHeight - window.innerHeight); //normalise b/w 0 to 1 val - min / max - min
-//     //   plane.rotation.z = document.documentElement.scrollTop/100; //normalise b/w 0 to 1 val - min / max - min
-//     //   plane.rotation.x = document.documentElement.scrollTop/100; //normalise b/w 0 to 1 val - min / max - min
-//     //   // planeMaterial.color.r = 0.4-document.documentElement.scrollTop/100;
-//     //   // planeMaterial.color.b = 0.3-document.documentElement.scrollTop/100;
-//     //   // planeMaterial.color.g = 0.6-(scrollElement.scrollTop-0)*0.5/(document.documentElement.scrollHeight - window.innerHeight);
-//     //   planeMaterial.color.setHSL((scrollElement.scrollTop-0)/(document.documentElement.scrollHeight - window.innerHeight), 1.0, 0.3);
+}
+// for testing
 
-//     // }
-    
-//     // // console.log(document.getElementsByClassName("mainText").scrollTop);
-//     camera.attributes.position.needsUpdate = true;
-//     renderer.render(scene, camera);
-//   }());
+showElement('projects')
 
-//   function onResize() {
-//     camera.aspect = window.innerWidth / window.innerHeight;
-//     camera.updateProjectionMatrix();
+function hideElement(el) {
+    document.getElementById(el + '-popup').style.display='none';
+    document.getElementById('popup-overlay').style.display='none'
+}
 
-//     renderer.setSize( window.innerWidth, window.innerHeight );
-//     // requestAnimationFrame(render);
-//     console.log("FIRED")
-//   }
+document.addEventListener('keydown', function(e) {
+    if (e.key === "Escape") {
+        closeAll();
+    }
+});
 
-//   function randomMeshColour() {
-//       planeMaterial.color.r = Math.random();
-//       planeMaterial.color.g = Math.random();
-//       planeMaterial.color.b = Math.random();
-//   }
+function closeAll() {
+    document.querySelectorAll('.popup').forEach(div => {
+        div.style.display = 'none';
+    })
+    document.getElementById('popup-overlay').style.display = 'none';
+        // document.getElementById('about-popup').style.display = 'none';
+}
 
-//   var timeoutFunction;
 
-//   window.onresize = function() {
-//     clearTimeout(timeoutFunction);
+/* writing in markdown
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Marked is:", marked);
+  const markdownBlocks = document.querySelectorAll('.markdown');
 
-//     timeoutFunction = setTimeout(function(){
-//       onResize();
-//     }, 100);
-//   }
-
-//   function sigmoid(z) {
-//     return 1 / (1 + Math.exp(-z/2));
-//   }
-
-//   var scrollTimeout;
-//   window.onscroll = function() {
-//     clearTimeout(scrollTimeout);
-//     scrollTimeout = setTimeout(function() {
-//       // randomMeshColour();
-//     }, 100);
-//   }
+  markdownBlocks.forEach(block => {
+    const raw = block.innerText;
+    block.innerHTML = marked.parse(raw);
+    console.log(marked.parse(raw))
+  });
+});*/
